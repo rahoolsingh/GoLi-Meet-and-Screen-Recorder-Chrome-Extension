2feb9c75d32d4b3dbbfb43c5d9c0a850db636a58
Veer Rajpoot <rahulksingh3907@gmail.com>
2024-11-18 20:19:27 +0530

    Replace Recorder component with Dashboard for enhanced layout and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import "./App.css";
-import Recorder from "./components/Recorder";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
     return (
-        <>
-            <Recorder />
-        </>
+        <div className="flex justify-center items-center bg-gradient-to-br from-green-700 via-green-900 to-cyan-950 text-white h-lvh">
+            <Dashboard />
+        </div>
     );
 }
 
